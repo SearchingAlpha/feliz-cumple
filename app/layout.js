7aeb4bf15a4d3e8c4d5d247b321b40c0ae359188
pablomv <morvegpablo@gmail.com>
2025-04-02 21:59:57 +0200

    commit: webpage metadata

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,20 @@
 // app/layout.js
 import './globals.css';
+import favicon from '@/public/favicon.svg'
 
 export const metadata = {
-  title: 'Retro Gaming Hub',
-  description: 'A retro pixel art gaming hub with a girly twist',
+  title: 'Feliz Cumple',
+  description: 'Un portal de juegos retro con estilo pixelado femenino',
+  icons: {
+    icon: '/favicon.svg',
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href='/favicon.svg' type="image/svg+xml" />
         {/* Import pixel font from Google Fonts */}
         <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
       </head>
